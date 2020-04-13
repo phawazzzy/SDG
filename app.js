49e25b13236c1ded6b889df0a5dd24c88d0c1151
kareem fawas <phawazzzy@gmail.com>
2020-04-13 17:16:48 +0100

    trial

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/v1/on-covid-19', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/api/v1/on-covid-19/', async (req, res) => {
+router.post('/', async (req, res) => {
   const use = await Estimator(req.body);
   res.status(200).json({
     data: use.data,
@@ -19,7 +19,7 @@ router.post('/api/v1/on-covid-19/', async (req, res) => {
   });
 });
 
-router.post('/api/v1/on-covid-19/json', async (req, res) => {
+router.post('/json', async (req, res) => {
   const use = await Estimator(req.body);
   res.status(200).json({
     data: use.data,
@@ -28,7 +28,7 @@ router.post('/api/v1/on-covid-19/json', async (req, res) => {
   });
 });
 
-router.post('/api/v1/on-covid-19/xml', async (req, res) => {
+router.post('/xml', async (req, res) => {
   const use = await Estimator(req.body);
   res.header('content-Type', 'routerlication/xml; charset=UTF-8');
   res.send(jsonToXml(use));
